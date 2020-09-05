ca15cd9c1c06c4e89115c602ae2b82ea74885172
Samia <samiawasim70@gmail.com>
2020-09-05 01:31:24 -0400

    Implemented oAuth2 with spotify

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
+import Login from './Login.js';
 import './App.css';
+import { getTokenFromUrl } from './spotify'
 
 function App() {
+
+  //Run code based on a given condition
+  useEffect(() => {
+    const token = getTokenFromUrl();
+    window.location.hash = "";
+    console.log('I have a token' , token);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+
+      <Login />
+
     </div>
   );
 }
---
+++ src/spotify.js
@@ -0,0 +1,39 @@
+// API file
+
+//https://developer.spotify.com/
+// https://developer.spotify.com/documentation/web-playback-sdk/quick-start/
+
+// Using oAuth2 authentication using spotify
+// Click login button
+// Redirect to spotify login page
+// Redirect to home page once logged in
+
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+
+const redirectUri = "http://localhost:3000/";
+const clientId = "a0e13b44177343328e1c8dfdb5d252ad";
+
+// Permission assigned to the application 'Spotify Authorization Scopes'
+const scopes = [
+    "user-read-currently-playing",
+    "user-read-recently-played",
+    "user-read-playback-state",
+    "user-top-read",
+    "user-modify-playback-state",
+];
+
+export const getTokenFromUrl  = () => {
+    return window.location.hash
+    .substring(1)
+    .split('&')
+    .reduce((initial,item) => {
+        // [accessToken=mysupersecretkey, name=sonny]
+        // #accessToken=mysupersecretkey&name=sonny&
+        let parts = item.split('=');
+        initial[parts[0] ] = decodeURIComponent(parts[1]);
+
+        return initial;
+    } , {});
+}
+
+export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join('%20')}&response_type=token&show_dialog=true`;
